7b33d076efe59f85ba513c164d057a43404ca741
Gujja <ngujja@wrbts.ads.wrberkley.com>
2020-03-18 23:08:31 -0500

    add documentation and prompt for file location

---
+++ src/Prepare_To_Run.md
@@ -0,0 +1,41 @@
+# Prepare to run 
+Follow these instructions to prepare to edit the excel sheet and generate json file that will be used by calendar app
+
+First browse this github and go to 
+```$xslt
+src\main\resources
+``` 
+folder and download following files
+```$xslt
+csvtojson-1.0-SNAPSHOT-jar-with-dependencies.jar
+run.cmd
+jdk8
+events.csv
+events.json
+```
+
+Double click **events.csv**, this will open a Microsoft Excel. Edit your entries and save it
+ 
+## How to run
+
+####events.csv
+Edit _events.csv_ file in Microsoft Excel and save it. Make sure event is 
+either of these values **breakfast**, **lunch**, **announcement**, **snack**.
+Make sure date format is **mm/dd/yyyy** e.g **3/11/2020**. Entry in **Description** will appear
+in the calendar app, make sure no **,** is used when filling out the description.
+Save _events.csv_ file after editing and now you are ready to create _events.json_ file based
+on the _events.csv_ file
+
+Double click the  
+```
+run.cmd
+```
+from your local windows folder, this will open a command shell and will prompt you with
+```$xslt
+Type in events.csv file directory location Eg: C:\cvs and press ENTER
+```
+Type the location of the events.csv file on your local machine. Once the run completes it will create a file
+```$xslt
+events.json
+```
+in the location you typed in earlier. Upload the events.json file to **website** github repo
--- src/main/java/CsvToJson.java
+++ src/main/java/CsvToJson.java
@@ -1,31 +1,57 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class CsvToJson {
-    public static void main(String ... args) throws IOException {
-        File input = new File("C://dev//events.csv");
-        File output = new File("C://dev//events.json");
-        Pattern pattern = Pattern.compile(",");
-        try (BufferedReader in = new BufferedReader(new FileReader(input));) {
-            List<Event> weeks = in
-                    .lines()
-                    .skip(1)
-                    .map(line -> {
-                        String[] x = pattern.split(line);
-                        return new Event(x[0],x[1],x[2], x[0]+"-"+x[2]);
-                    })
-                    .collect(Collectors.toList());
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.enable(SerializationFeature.INDENT_OUTPUT);
-            FileWriter fileWriter = new FileWriter(output);
-            mapper.writeValue(fileWriter, weeks);
+    public static void main(String ... args) {
+        boolean toContinue = true;
+        Scanner scanner = new Scanner(System.in);
+        try {
+            while (toContinue) {
+                System.out.println("Type in events.csv file directory location Eg: C:\\cvs and press ENTER");
+                String csvLocation = scanner.nextLine();
+                toContinue = false;
+                String csvDir = FilenameUtils.normalize(FilenameUtils.separatorsToSystem(csvLocation));
+                if (!Files.isDirectory(Paths.get(csvDir))) {
+                    System.out.println("events.csv file does not exist at " + csvDir + "\n");
+                    System.out.println("Please make sure events.csv file is placed at " + csvDir + " and try again");
+                    return;
+                } else {
+                    File input = new File(csvDir + File.separator + "events.csv");
+                    File output = new File(csvDir + File.separator + "events.json");
+                    Pattern pattern = Pattern.compile(",");
+                    try (BufferedReader in = new BufferedReader(new FileReader(input));) {
+                        List<Event> weeks = in
+                                .lines()
+                                .skip(1)
+                                .map(line -> {
+                                    String[] x = pattern.split(line);
+                                    return new Event(x[0],x[1],x[2], x[0]+"-"+x[2]);
+                                })
+                                .collect(Collectors.toList());
+                        ObjectMapper mapper = new ObjectMapper();
+                        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+                        FileWriter fileWriter = new FileWriter(output);
+                        mapper.writeValue(fileWriter, weeks);
+                        System.out.println(output.toString() + " File is saved");
+                    } catch (IOException e) {
+                        System.out.println("events.csv file does not exist at " + csvDir + "\n");
+                        System.out.println("Please make sure events.csv file is placed at " + csvDir + " and try again");
+                    }
+                }
+            }
+        } catch (IllegalStateException | NoSuchElementException e) {
+            System.out.println("System.in closed; exiting");
         }
-
     }
 
 }
